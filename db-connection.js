d137f8ea24ab3975207b24806ee727ab6a5cc9f6
Mitesh Modi <mitesh.modi003@gmail.com>
2020-08-14 14:31:40 -0400

    updated db

--- db-connection.js
+++ db-connection.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
 const config = require('./config.json');
-const dotenv = require('dotenv');
-dotenv.config();
 
 module.exports = () => {
-    mongoose.connect(process.env.MONGODB_URI || `mongodb+srv://${process.env.USER_NAME}:${process.env.DB_PASSWORD}@funcept.tz1wo.mongodb.net/test`, {
+    mongoose.connect(process.env.MONGODB_URI || config.connectionString, {
         useCreateIndex: true,
         useNewUrlParser: true,
         poolSize: 5,
