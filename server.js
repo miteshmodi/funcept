ecc160beefd1ad6648d7de2fb63bee72abddc241
Mitesh Modi <mitesh.modi003@gmail.com>
2020-08-14 12:20:07 -0400

    server js file change

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'public', 'index.html'));
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
